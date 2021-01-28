41f103d16392aea98b63e1201bbe8a37b510b8ca
Fouad Saikali <fouad@ecreators.com.au>
2021-01-28 11:29:37 +1100

    add ES lang

---
+++ lang/es/theme_learnbook.php
@@ -0,0 +1,153 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Learnbook - en language file
+ *
+ * @package    theme_learnbook
+ * @author     2019 Chris Megahan, eCreators <chris@ecreators.com.au>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Learnbook';
+$string['choosereadme'] = 'Learnbook';
+
+// Settings
+$string['configtitle'] = 'Ajustes de Learnbook';
+
+// General settings
+$string['generalsettings'] = 'Ajustes generales';
+$string['preset'] = 'Tema preestablecido';
+$string['preset_desc'] = 'Elige un preajuste para cambiar la imagen del tema en general.';
+$string['presetfiles'] = 'Archivos preestablecidos de temas adicionales';
+$string['presetfiles_desc'] = 'Los archivos preestablecidos se pueden utilizar para alterar drásticamente la apariencia del tema. Consulte el tema preestablecidos <ahref=https://docs.moodle.org/dev/Boost_Presets>Boost</a> para obtener información sobre cómo crear y compartir sus propios archivos preestablecidos, y consulte <a href = http: //moodle.net / boost> Repositorio de preestablecidos </a> para ver preestablecidos que otros han compartido.';
+$string['brandcolor'] = 'Color de la marca';
+$string['brandcolor_desc'] = 'Color del acento.';
+// Alternative home page for learnbook.
+$string['use_alternate_home_page'] = 'Usar página principal alternativa';
+$string['use_alternate_home_page_desc'] = 'Esto redirigirá a los usuarios a la página de inicio de la tienda de comercio electrónico si el comercio electrónico está habilitado y SSO está deshabilitado.';
+
+// Common settings
+$string['commonsettings'] = 'Ajustes comunes';
+$string['headercolor'] = 'Color del encabezado';
+$string['headercolor_desc'] = 'El color será aplicado al fondo del encabezado del sitio.';
+$string['headertextcolor'] = 'Color del texto del encabezado';
+$string['headertextcolor_desc'] = 'El color será aplicado al texto en el encabezado del sitio.';
+$string['leftmenuheadercolor'] = 'Color del encabezado del panel de Learnbook';
+$string['leftmenuheadercolor_desc'] = 'El color será aplicado al encabezado del panel de learnbook.';
+$string['leftmenuheadertextcolor'] = 'Color del texto del encabezado del panel de Learnbook';
+$string['leftmenuheadertextcolor_desc'] = 'El color se aplicará al texto del encabezado del panel de Learnbook';
+$string['leftmenuitemhovercolor'] = 'Color de "hover" de elementos de menú del panel Learnbook';
+$string['leftmenuitemhovercolor_desc'] = 'El color se aplicará al fondo de los elementos del menú cuando el puntero se ubique sobre ellos.';
+$string['leftmenuitemstextcolor'] = 'Color del texto de los elementos del menú del panel Learnbook';
+$string['leftmenuitemstextcolor_desc'] = 'El color se aplicará al texto de los elementos del menú';
+$string['leftmenumainitemcolor'] = 'Color de fondo de los elementos del menú principal del panel Learnbook';
+$string['leftmenumainitemcolor_desc'] = 'El color se aplicará como color de fondo de los elementos del menú';
+$string['leftmenusubitemcolor'] = 'Color de fondo de los elementos del submenú del panel Learnbook';
+$string['leftmenusubitemcolor_desc'] = 'El color se aplicará como color de fondo de los elementos del submenú';
+$string['footercolor'] = 'Color de fondo del pie de página';
+$string['footercolor_desc'] = 'El color se aplicará como color de fondo al pie de página.';
+$string['sectionheadercolor'] ='Color de encabezado de sección';
+$string['sectionheadercolor_desc'] = 'El color se aplicará como fondo del encabezado de la sección.';
+//$string['sectionhovercolor'] = 'Color de "hover" de sección';
+//$string['sectionhovercolor_desc'] = 'El color se aplicará como fondo al botón en secciones cuando el puntero se ubique sobre ellas.';
+$string['coursetilecolor'] = 'Color de fondo del mosaico de Mis equipos';
+$string['coursetilecolor_desc'] = 'El color se aplicará como fondo del mosaico de equipos';
+$string['coursetiletextcolor'] = 'Color del texto del mosaico del curso';
+$string['coursetiletextcolor_desc'] = 'El color se aplicará al texto en un mosaico de curso';
+$string['coursebtncolor'] = 'Esquema de colores de bloques de Learnbook';
+$string['coursebtncolor_desc'] = "El color de la marca para el bloque Mis cursos y Mis equipos";
+$string['coursebtnhovercolor'] = 'Color de "hover" del botón Ingresar al Curso';
+$string['coursebtnhovercolor_desc'] = 'El color se aplicará al pasar el cursor sobre el botón Ingresar al Curso';
+$string['login_page_template'] = 'Seleccionar plantilla de página de inicio de sesión';
+
+// Logo Settings
+$string['logosettings'] = 'Logo de ajustes';
+$string['favicon'] = 'Favicon';
+$string['favicon_desc'] = 'Sube tu icono de favicon aquí';
+
+// Login Page Settings
+$string['loginpagesettings'] = 'Configuración de la página de inicio de sesión';
+$string['display_welcome_text'] = "Mostrar texto de bienvenida";
+$string['display_welcome_text_desc'] = "Desmarque para ocultar el texto de bienvenida de la plantilla Apollo y Learnbook (no se aplica a la plantilla Athena)";
+$string['welcometitle'] = "Título de bienvenida";
+$string['welcometitle_desc'] = "Título que se mostrará para el mensaje de bienvenida en la página de inicio de sesión (se permite un máximo de 30 caracteres)";
+$string['welcomemsg'] = "Mensaje de bienvenida";
+$string['welcomemsg_desc'] = "Mensaje de bienvenida que se mostrará en la página de inicio de sesión";
+$string['welcome_text_location'] = "Ubicación del texto de bienvenida";
+$string['welcome_text_location_desc'] = "Mostrar texto de bienvenida en el lado izquierdo o derecho de la plantilla de Athena o Apollo, la vista móvil puede afectar este cambio (no se aplica a la plantilla Learnbook).";
+$string['left_side'] = "Lado izquierdo";
+$string['right_side'] = "Lado derecho";
+$string['welcomeimg'] = "Imagen de bienvenida";
+$string['welcomeimg_desc'] = "Imagen de fondo del mensaje de bienvenida en la página de inicio de sesión (no se aplica a la plantilla Apollo y Learnbook)";
+$string['slideshow'] =  'Imagen de diapositiva de inicio de sesión';
+$string['slideshowdesc'] = 'Seleccionar imagen para la presentación de diapositivas de la página de inicio de sesión';
+$string['login_page_template_desc'] = 'Seleccione la plantilla para la página de inicio de sesión.';
+$string['learnbook_template'] = 'Plantilla Learnbook';
+$string['athena_template'] = 'Plantilla Atenea';
+$string['apollo_template'] = 'Plantilla Apolo';
+$string['logintitle'] = 'Título del formulario de inicio de sesión';
+$string['logintitle_desc'] = 'Texto del título que se mostrará en el formulario de inicio de sesión (se permite un máximo de 40 caracteres).';
+$string['logintitle_default'] = 'Iniciar sesión';
+
+// Advanced Settings
+$string['advancedsettings'] = 'Ajustes avanzados';
+$string['rawscsspre'] = 'SCSS inicial sin procesar';
+$string['rawscsspre_desc'] = 'En este campo puede proporcionar el código SCSS de inicialización, se inyectará antes que todo lo demás. La mayoría de las veces utilizará esta configuración para definir variables.';
+$string['rawscss'] = 'SCSS sin procesar';
+$string['rawscss_desc'] = 'Utilice este campo para proporcionar código SCSS o CSS que se inyectará al final de la hoja de estilo.';
+
+// Login form
+$string['or'] = 'O iniciar sesión con';
+$string['guest_login_desc'] = '¿Quieres explorar el sitio? Inicia sesión como invitado ';
+$string['guest_login'] = 'Iniciar sesión como invitado';
+$string['sign_up'] = 'Registrarse';
+$string['capslock_help'] = 'Las mayúsculas están activadas';
+$string['resetfields'] = '¿Has olvidado tu contraseña?';
+
+// The learnbook user profile settings
+$string['learnbook_user_profile'] = 'Habilitar la redirección del perfil de usuario de Learnbook';
+$string['learnbook_user_profile_desc'] = 'Alternar para activar/desactivar la redirección del perfil de usuario de Learnbook.';
+
+//footer settings
+$string['disabled'] = 'Desactivar';
+$string['moodledocs'] = 'Enlace de Moodle Docs';
+$string['moodledocsdesc'] = 'Mostrar el enlace de Moodle Docs en el pie de página.';
+$string['layoutcheck'] = 'Verifique su diseño';
+$string['layoutcheckdesc'] = 'Utilice la herramienta a continuación para comprobar el número de bloques que ha utilizado y ver una representación de su nuevo diseño.';
+$string['layoutcount1'] = 'Puede establecer un máximo de ';
+$string['layoutcount2'] = 'Bloquear regiones. Actualmente está utilizando: ';
+$string['layoutaddcontent'] = '¿Estás contento con tu diseño? Ahora agrega contenido a tus bloques.';
+$string['layoutaddcontentdesc'] = 'Utilice html si desea personalizar su texto.';
+$string['layoutsettings'] = 'Configuración de pie de página';
+$string['footersettings'] = 'Pie de página';
+$string['footersettingsheading'] = 'Establecer el contenido que debe aparecer en el pie de página.';
+$string['footerdesc'] = 'Controla el contenido que aparece en las secciones de 4 pies de página de la página.';
+$string['showfooterblocks'] = 'Mostrar bloque de pie de página';
+$string['showfooterblocksdesc'] = 'Mostrar / ocultar el bloque de pie de página inferior utilizado para notas de pie de página / región de documentación de Moodle';
+$string['moodlefooterinfo'] = 'Enlaces estándar de Moodle';
+$string['moodlefooterinfodesc'] = 'Mostrar los enlaces estándar de moodle en el pie de página.';
+$string['footerlayoutrow'] = 'Creador de diseño de pie de página';
+$string['footerlayoutrowdesc'] = 'Cree su diseño para regiones de bloques de pie de página.';
+$string['footnote'] = 'Nota de Pie de página';
+$string['footnotedesc'] = 'Agregar texto al pie de página.';
+$string['footercontent'] = 'Sección de contenido de pie de página';
+$string['footercontentdesc'] = 'Agregar contenido a la sección de pie de página';
+$string['hide'] = 'Ocultar';
+$string['show'] = 'Mostrar';
+
